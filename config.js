f2a4902492feaea9b957aac26762883ce309963c
Estevamsl <estevamsouzalaureth@gmail.com>
2022-10-17 12:59:40 -0300

    :sparkles: new feature

---
+++ config.js
@@ -0,0 +1,10 @@
+// TODO: hash路由支持
+module.exports = {
+  client:{
+
+  },
+  server:{
+    baseURL: 'http://127.0.0.1:3001', // 服务端渲染时，http请求的host地址,比如 http://127.0.0.1:3001
+  },
+  outputPath: './dist',
+};
