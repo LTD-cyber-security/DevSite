27607a26cbd8a398eed0494b5f315d51f011e3ca
Estevam Souza <estevamsouzalaureth@gmail.com>
2023-09-14 16:40:14 -0300

    :boom: removendo arquivos desnecessarios

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-var config = {
-  env: process.env.NODE_ENV,
-  apiEnv: process.env.APP_ENV,
-  apiUrl: process.env.API_URL,
-  blogId: process.env.BLOG_ID
-};
-
-module.exports = config;
--- src/server/client/tools/http.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const http = axios.create({
-  baseURL: process.env.BASE_URL,
-  timeout: 5000,
-});
-
-export default http;
