cace5620ef2bee518e26bbab93e536eed6fa7400
Estevam Souza <estevamsouzalaureth@gmail.com>
2023-09-14 17:13:51 -0300

    :boom: removendo arquivos desnecessarios

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,20 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+    $to = "estevamsouzalaureth@gmail.com"; // Endereço de e-mail de destino
+    $headers = "From: $email";
+    $message_body = "Nome: $name\nEmail: $email\nAssunto: $subject\nMensagem:\n$message";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    if (mail($to, "Formulário de Contato", $message_body, $headers)) {
+        echo "Sua mensagem foi enviada com sucesso. Obrigado!";
+    } else {
+        echo "O envio da mensagem falhou. Por favor, tente novamente mais tarde.";
+    }
+} else {
+    echo "Erro: Este script PHP deve ser acessado através do método POST.";
+}
 ?>
