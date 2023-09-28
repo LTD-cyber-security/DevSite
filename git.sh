3ee4035f4c4d7f4fd1cf6513190e4a13b3d430f1
estevam5s <estevamsouzalaureth@gmail.com>
2023-09-28 20:07:10 -0300

    :tada

--- git.sh
+++ git.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Auto Push
+git config --global user.email ""
+git config --global user.name ""
 
 
-# git init
-# git remote add origin git@github.com:setdjod/blogger-api-netlify.git
+git init
+git remote add origin git@github.com:setdjod/blogger-api-netlify.git
 git pull
 git add .
 git commit -m "Update"
