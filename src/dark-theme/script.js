143d5a15ef900ba0e74e6830b7d8fc1031c24f0c
estevam5s <estevamsouzalaureth@gmail.com>
2023-10-01 03:34:57 -0300

    :boom: nova feature

--- src/dark-theme/script.js
+++ src/dark-theme/script.js
@@ -3,3 +3,14 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', () => {
   document.body.classList.toggle('dark')
 })
+
+const checkbox = document.getElementById('dark-mode-toggle');
+const label = document.querySelector('.label');
+
+checkbox.addEventListener('change', function () {
+  if (this.checked) {
+    label.classList.add('rotate');
+  } else {
+    label.classList.remove('rotate');
+  }
+});
